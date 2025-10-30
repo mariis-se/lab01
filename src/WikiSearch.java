8bac303172ee7d970bc05e7f97874a92c6e16a01
Mariia <mvs6568708@gmail.com>
2025-10-30 12:14:52 +0300

    fetch search start commit

--- src/WikiSearch.java
+++ src/WikiSearch.java
@@ -1,6 +1,12 @@
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.time.Duration;
 import java.util.Scanner;
 import java.io.IOError;
 import java.util.IllegalFormatException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -42,6 +48,30 @@ public class WikiSearch {
 
     }
 
+    private static String fetchSearch(String search) {
+        try{ // кодируем запрос для дальнейшей передачи в url
+            String searchURL = URLEncoder.encode(search, StandardCharsets.UTF_8);
+            String url = wikiApi_URL +  "?action=query&list=search&utf8=&format=json&srsearch=" + searchURL;
+            // создаем http клиент с таймаутом
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).timeout(Duration.ofSeconds(timeout)).GET().build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        } catch (UnsupportedEncodingException e){
+            System.out.println("Ошибка кодирования запроса " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Ошибка сети " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println("Запрос прерван " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Неверный URL" + e.getMessage());
+        }
+        return null;
+
+    }
 
 
 
