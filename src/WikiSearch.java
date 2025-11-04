d135edaffebb9b20f128b429344a1697cc8d1fee
Mariia <mvs6568708@gmail.com>
2025-11-04 20:36:03 +0300

    fetch search main sceleton)) start
    commit

--- src/WikiSearch.java
+++ src/WikiSearch.java
@@ -21,44 +21,71 @@ public class WikiSearch {
 
 
     public static void main(String[] s){
-        Scanner scanner = new Scanner(System.in);
-        String search = "";
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+        //String search = "";
+        while(true) {
+            try {
+                System.out.print("Введите поисковой запрос: ");
+                String search = scanner.nextLine();
 
-        try {
-            System.out.print("Введите поисковой запрос: ");
-            search = scanner.nextLine();
+                if (search.equalsIgnoreCase("0")){
+                    break;
+                }
 
-            //trim - убираем пробелы
-            if (search.trim().isEmpty()) {
-                System.out.println("Ошибка. Пустой запрос.");
-                return;
+                //trim - убираем пробелы
+                if (search.trim().isEmpty()) {
+                    System.out.println("Ошибка. Пустой запрос.");
+                    continue;
+                }
+
+                System.out.println("Searching.......");
+
+                String jsonAnswer = fetchSearch(search);
+//                if (jsonAnswer != null){
+//
+//                }
+
+
+                System.out.println("Считано: " + search);
+            } catch (IOError e) {
+                System.out.println("Ошибка ввода(вывода)!");
+            } catch (IllegalFormatException e) {
+                System.out.println("Ошибка формата ввода!");
             }
-
-            System.out.println("Считано: " + search);
-        } catch (IOError e) {
-            System.out.println("Ошибка ввода(вывода)!");
-        } catch (IllegalFormatException e) {
-            System.out.println("Ошибка формата ввода!");
-        }
 //        finally {
 //            if (scanner != null) {
 //                scanner.close();
 //            }
 //        }
+        }
+        scanner.close();
+        System.out.println("The end of the prog");
 
     }
 
     private static String fetchSearch(String search) {
         try{ // кодируем запрос для дальнейшей передачи в url
             String searchURL = URLEncoder.encode(search, StandardCharsets.UTF_8);
-            String url = wikiApi_URL +  "?action=query&list=search&utf8=&format=json&srsearch=" + searchURL;
+            String url = wikiApi_URL +  "?action=query&list=search&utf8=&format=json&srsearch=" + searchURL + "&srlimit=10";
             // создаем http клиент с таймаутом
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).timeout(Duration.ofSeconds(timeout)).GET().build();
+
+            HttpClient client = HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(timeout))
+                    .build();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .timeout(Duration.ofSeconds(timeout))
+                    .GET()
+                    .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            return response.body();
+            if (response.statusCode() == 200) {
+                return response.body();
+            } else {
+                System.out.println("Ошибка HTTP: " + response.statusCode());
+            }
 
         } catch (UnsupportedEncodingException e){
             System.out.println("Ошибка кодирования запроса " + e.getMessage());
